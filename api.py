a60bf9652e33f78f554c6d4c3e2d310f3061428b
choieastsea <46194004+choieastsea@users.noreply.github.com>
2024-02-18 20:45:28 +0900

    [FEAT] sorting and pagination

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from model import Account
-from schema import AccountCreateDto, AccountResponseDto
+from schema import *
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import text, select
+from sqlalchemy import text, select, between, desc
 from database import db
 
 api_router = APIRouter(prefix='/api')
@@ -16,9 +16,24 @@ async def health_check(session: AsyncSession = Depends(db.get_db)):
 
 
 @api_router.get('/accounts/', response_model=list[AccountResponseDto])
-async def list(session: AsyncSession = Depends(db.get_db)) -> list[Account]:
-    all_accounts = await session.scalars(select(Account).filter(Account.is_activated == True))
-    return all_accounts
+async def list(sort: SortQueryBasic = Depends(), pagination: PaginationQueryBasic = Depends(), session: AsyncSession = Depends(db.get_db)) -> list[Account]:
+    all_accounts_query = select(Account)
+    # 1. sort
+    if sort.sort_by and not hasattr(Account, sort.sort_by):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f'there is no field named {sort.sort_by} in {Account.__tablename__}')
+    model_field = getattr(Account, sort.sort_by)
+    sort_query = all_accounts_query.order_by(desc(
+        model_field)) if sort.order_by == 'desc' else all_accounts_query.order_by(model_field)
+    # TODO 2. filter
+    filter_query = sort_query
+    # 3. pagination
+    strt_idx = (pagination.page-1) * pagination.per_page + 1
+
+    paged_accounts_query = filter_query.offset(
+        strt_idx).limit(pagination.per_page)
+    accounts = await session.scalars(paged_accounts_query)
+    return accounts
 
 
 @api_router.post('/accounts/', response_model=AccountResponseDto)
--- schema.py
+++ schema.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, Field, ConfigDict
-from typing import Optional
+from typing import Optional, Literal
 from datetime import datetime, date
+from dataclasses import dataclass
 
 
 class AccountCreateDto(BaseModel):
@@ -16,3 +17,23 @@ class AccountResponseDto(BaseModel):
     description: str
     created_at: datetime
     updated_at: datetime
+
+
+class PaginationQueryBasic(BaseModel):
+    """
+    args
+        * page : requested page
+        * per_page : count of items in a single page
+    """
+    page: Optional[int] = Field(default=1, ge=1)
+    per_page: Optional[int] = Field(default=10, ge=1)
+
+
+class SortQueryBasic(BaseModel):
+    """
+    args
+        * sort_by : field name of model
+        * order_by : 'asc' or 'desc'
+    """
+    sort_by: Optional[str] = Field(default=None, min_length=1)
+    order_by: Optional[Literal['asc', 'desc']] = Field(default=None)
