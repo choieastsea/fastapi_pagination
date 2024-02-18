2695bbfd2af12dccf5dac9dcd4496f950e3708da
choieastsea <46194004+choieastsea@users.noreply.github.com>
2024-02-18 20:36:31 +0900

    [FEAT] add fixture datas

--- main.py
+++ main.py
@@ -2,16 +2,31 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from config import get_setting
 from database import db, Base
+from sqlalchemy.ext.asyncio import AsyncConnection
+from sqlalchemy import insert
+from model import Account
 from api import api_router
+import uuid
 
 SETTINGS = get_setting()
+
+
+async def add_default_accounts(conn: AsyncConnection, cnt: int = 100):
+    params = []
+    for _ in range(cnt):
+        params.append(
+            {'name': str(uuid.uuid4())[:20], 'nickname': str(uuid.uuid4())[:20], 'description': str(uuid.uuid4())})
+    await conn.execute(insert(Account), params)
+    await conn.commit()
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     db.init_db(SETTINGS.DB_URL)
-    async with db.engine.connect() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with db.engine.connect() as conn:
+    #     await conn.run_sync(Base.metadata.drop_all)
+    #     await conn.run_sync(Base.metadata.create_all)
+    #     await add_default_accounts(conn)
     yield
     await db.disconnect()
 
